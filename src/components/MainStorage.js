8e4853e734a8e3366efcafff165e2f217c172723
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-09-29 15:47:18 +0300

    main storage first commit

--- src/components/MainStorage.js
+++ src/components/MainStorage.js
@@ -3,9 +3,15 @@ import React from 'react';
 export const MainStorage = () => {
     return (   
         <div className='container'>
-            <div className='row'>
-                <div className='col-sm-12'>
-                    <h1>Storage</h1>
+            <div className='row mt-5'>
+                <div className='col-sm-12 col-md-4 bg-dark'>
+                    <div className='row'>
+                        <div className='col-sm-12'>
+                            <button className='btn bg-primary text-white mt-3'>
+                                <span className=''> </span>
+                                Add New</button>
+                        </div>
+                    </div>
                 </div> 
             </div>   
         </div>
