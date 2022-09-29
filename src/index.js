112930c61f157ce549b7b30c5efad86246f92514
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-09-29 19:22:17 +0300

    file declaration updated

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import {Navbar} from './components/Navbar';
-import {Home} from './components/Home';
-import Login from './components/Login.js';
-import Signup from './components/Signup.js';
-import Forgot from './components/Forgot.js';
 import { MainStorage } from "./components/MainStorage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
